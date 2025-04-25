ab6afb12f7aef7b56bb4cf37c11e9e71ed87b58e
Vlad Mantur <Colardra1@gmail.com>
2025-04-25 22:12:33 +0300

    Created simple wordly template
    Created static word for game
    Created lose and win messages
    Created word cheking
    Created keydown listener for word input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,84 @@
+import { useEffect, useState } from "react";
+import WordlyWindow from "./assets/components/WordlyWindow";
+
+const MAX_ATTEMPTS = 6;
+
 function App() {
-  return <></>;
+  const [correctWord, setCorrectWord] = useState<string | null>(null);
+  const [words, setWords] = useState<string[]>([]);
+  const [currentAttempt, setCurrentAttempt] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+
+  useEffect(() => {
+    setCorrectWord("HELLO");
+  }, []);
+
+  useEffect(() => {
+    const pressKey = (e: KeyboardEvent) => {
+      if (gameOver) return;
+
+      if (e.key.match(/^[a-zA-Z]$/)) {
+        if (words[currentAttempt]?.length === correctWord?.length) return;
+        const key = e.key.toUpperCase();
+
+        words[currentAttempt] = words[currentAttempt]
+          ? words[currentAttempt] + key
+          : key;
+
+        setWords([...words]);
+      }
+
+      if (e.key === "Backspace") {
+        words[currentAttempt] = words[currentAttempt]
+          ? words[currentAttempt].slice(0, -1)
+          : "";
+        setWords([...words]);
+      }
+
+      if (e.key === "Enter") {
+        if (correctWord === words[currentAttempt]) {
+          console.log("YOU WIN!");
+          setCurrentAttempt(currentAttempt + 1);
+          setGameOver(true);
+          return;
+        }
+
+        if (currentAttempt === MAX_ATTEMPTS - 1) {
+          console.log("YOU LOSE!");
+          setCurrentAttempt(currentAttempt + 1);
+          setGameOver(true);
+          return;
+        }
+
+        if (
+          currentAttempt < MAX_ATTEMPTS &&
+          correctWord?.length &&
+          words[currentAttempt]?.length &&
+          words[currentAttempt].length === correctWord?.length
+        ) {
+          setCurrentAttempt(currentAttempt + 1);
+        }
+      }
+    };
+
+    window.addEventListener("keydown", pressKey);
+
+    return () => {
+      window.removeEventListener("keydown", pressKey);
+    };
+  }, [words, currentAttempt, correctWord]);
+
+  return (
+    <div className="flex justify-center">
+      <WordlyWindow
+        className="m-auto pt-20"
+        wordList={words}
+        maxAttempts={MAX_ATTEMPTS}
+        currentAttempt={currentAttempt}
+        correctWord={correctWord ?? ""}
+      ></WordlyWindow>
+    </div>
+  );
 }
 
 export default App;
--- src/assets/components/WordlyWindow.tsx
+++ src/assets/components/WordlyWindow.tsx
@@ -1,8 +1,70 @@
-interface WordlyWindowProps
-  extends Omit<React.ComponentPropsWithoutRef<"div">, "children"> {}
+import React from "react";
 
-let WordlyWindow: React.FC<WordlyWindowProps> = ({ className, ...rest }) => {
-  return <div className={`my-class ${className || ""}`} {...rest}></div>;
+interface WordlyWindowProps
+  extends Omit<React.ComponentPropsWithoutRef<"div">, "children"> {
+  wordList: string[];
+  correctWord: string;
+  currentAttempt?: number;
+  maxAttempts: number;
+}
+
+let WordlyWindow: React.FC<WordlyWindowProps> = ({
+  className = "",
+  wordList,
+  correctWord,
+  currentAttempt = 0,
+  maxAttempts,
+  ...rest
+}) => {
+  return (
+    <div className={`flex flex-col gap-2 ${className}`} {...rest}>
+      {Array.from({ length: maxAttempts }).map((_, i) => (
+        <WordLine
+          key={i}
+          correctWord={correctWord}
+          word={wordList[i] || ""}
+          cheked={currentAttempt > i}
+        />
+      ))}
+    </div>
+  );
 };
+
+interface WordLineProps {
+  word: string;
+  correctWord?: string;
+  cheked?: boolean;
+}
+
+const checkLetter = (word: string, correctWord: string, i: number) => {
+  if (word[i] === correctWord[i]) {
+    return "bg-green-500";
+  }
+
+  if (word[i] && correctWord.includes(word[i])) {
+    return "bg-yellow-500";
+  }
+
+  return "bg-gray-500";
+};
+
+let WordLine: React.FC<WordLineProps> = React.memo(
+  ({ word, correctWord, cheked }) => {
+    return (
+      <div className="flex gap-2">
+        {Array.from({ length: correctWord?.length || 5 }).map((_, i) => (
+          <div
+            key={i}
+            className={`flex size-12 items-center justify-center rounded border text-2xl font-bold ${
+              cheked && checkLetter(word, correctWord || "", i)
+            }`}
+          >
+            {word[i] || ""}
+          </div>
+        ))}
+      </div>
+    );
+  },
+);
 
 export default WordlyWindow;
